af7a8ad5bf992a155523925ab1b31e2c0f83c21f
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-07-22 04:12:29 +0700

    login admin

--- app/api/staff/route.tsx
+++ app/api/staff/route.tsx
@@ -3,6 +3,5 @@ import { staffService } from "../service/staff";
 export async function GET() {
     const data = await staffService.findAll();
     // const data = [{ name: "John Doe" }, { name: "Minh" }];
-
     return NextResponse.json(data);
 }
