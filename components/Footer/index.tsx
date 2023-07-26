8a003e19c44e024c3c3cc11547704e0010e4df82
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-07-26 20:24:27 +0700

    add footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
         <div className="container my-5">
+            <div id="fb-root"></div>
+<script async defer crossOrigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v17.0&appId=1114480602426398&autoLogAppEvents=1" nonce="Ldlu2aSe"></script>
             {/* Footer */}
             <footer className="text-center text-lg-start text-white" style={{ backgroundColor: '#929fba' }}>
                 {/* Grid container */}
@@ -22,18 +24,18 @@ const Footer = () => {
                             <hr className="w-100 clearfix d-md-none" />
                             {/* Grid column */}
                             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
+                                <h6 className="text-uppercase mb-4 font-weight-bold">Chuyên mục</h6>
                                 <p>
-                                    <a className="text-white">MDBootstrap</a>
+                                    <a className="text-white text-decoration-none">Thức ăn</a>
                                 </p>
                                 <p>
-                                    <a className="text-white">MDWordPress</a>
+                                    <a className="text-white text-decoration-none">Dinh dưỡng</a>
                                 </p>
                                 <p>
-                                    <a className="text-white">BrandFlow</a>
+                                    <a className="text-white text-decoration-none">Giảm cân</a>
                                 </p>
                                 <p>
-                                    <a className="text-white">Bootstrap Angular</a>
+                                    <a className="text-white text-decoration-none">Món lười</a>
                                 </p>
                             </div>
                             {/* Grid column */}
@@ -42,28 +44,17 @@ const Footer = () => {
                             <hr className="w-100 clearfix d-md-none" />
                             {/* Grid column */}
                             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                <p><i className="fas fa-home mr-3" /> New York, NY 10012, US</p>
-                                <p><i className="fas fa-envelope mr-3" /> info@gmail.com</p>
+                                <h6 className="text-uppercase mb-4 font-weight-bold">Liên hệ</h6>
+                                <p><i className="fas fa-home mr-3" />Quang Trung, Gò Vấp, HCM</p>
+                                <p><i className="fas fa-envelope mr-3" /> cancook@gmail.com</p>
                                 <p><i className="fas fa-phone mr-3" /> + 01 234 567 88</p>
                                 <p><i className="fas fa-print mr-3" /> + 01 234 567 89</p>
                             </div>
                             {/* Grid column */}
                             {/* Grid column */}
                             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 className="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-                                {/* Facebook */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#3b5998' }} href="#!" role="button"><i className="fab fa-facebook-f" /></a>
-                                {/* Twitter */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#55acee' }} href="#!" role="button"><i className="fab fa-twitter" /></a>
-                                {/* Google */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#dd4b39' }} href="#!" role="button"><i className="fab fa-google" /></a>
-                                {/* Instagram */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#ac2bac' }} href="#!" role="button"><i className="fab fa-instagram" /></a>
-                                {/* Linkedin */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#0082ca' }} href="#!" role="button"><i className="fab fa-linkedin-in" /></a>
-                                {/* Github */}
-                                <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#333333' }} href="#!" role="button"><i className="fab fa-github" /></a>
+                                <h6 className="text-uppercase mb-4 font-weight-bold">Theo dõi chúng tôi</h6>
+                                <div className="fb-page" data-href="https://www.facebook.com/profile.php?id=100095091395635" data-tabs="events" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/profile.php?id=100095091395635" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/profile.php?id=100095091395635">CanCook</a></blockquote></div>
                             </div>
                         </div>
                         {/*Grid row*/}
@@ -73,8 +64,8 @@ const Footer = () => {
                 {/* Grid container */}
                 {/* Copyright */}
                 <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                    © 2020 Copyright:
-                    <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+                    © 2023 Copyright: 
+                    <a className="text-white text-decoration-none" href="https://mdbootstrap.com/"> zuhot.github.com</a>
                 </div>
                 {/* Copyright */}
             </footer>
