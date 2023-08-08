e2f113d2d433f1c0082dc9eaca9b6964968e4c16
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-09 03:46:17 +0700

    test8

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXTAUTH_SECRET: 'seocranet', // Thay YOUR_SECRET_KEY bằng giá trị khóa bí mật thực sự
-    },
-}
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
