e3c651283f4561fbd5d0ef929f7f42c4199d2bbb
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-09 03:57:02 +0700

    test1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    },
-    // Các tùy chọn khác của next.config.js...
-};
-export default nextConfig
+const nextConfig = {}
+
+module.exports = nextConfig
