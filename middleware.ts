e47e29257a873ba421c9d7b6e0f5b6b560a3f1cb
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-09 04:56:47 +0700

    fix thanh cong

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import withAuth from "next-auth/middleware";
 
 // export {default} from "next-auth/middleware"
 export default withAuth(
-    async (req, res) => {
-        // Xử lý middleware tại đây
-    },
+
     {
         secret: 'seocranet',
     }
