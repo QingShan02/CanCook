20f515f30ae3288365b553a09fc0121ef346d1af
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-09 05:03:12 +0700

    fix thanh congtr

--- middleware.ts
+++ middleware.ts
@@ -7,4 +7,4 @@ export default withAuth(
         secret: 'seocranet',
     }
 );
-export const config = { matcher: ["/admin", "/admin/article", "/admin/**"] }
+export const config = { matcher: ["/admin", "/admin/article", "/admin/*"] }
