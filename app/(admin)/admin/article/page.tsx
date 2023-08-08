9a488039f280983d12d947565c39af832fd5efc4
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-08-09 04:10:34 +0700

    setup delete bài viết

--- app/(admin)/admin/article/page.tsx
+++ app/(admin)/admin/article/page.tsx
@@ -4,14 +4,15 @@ import Link from "next/link";
 
 const Article = () => {
   return (
-    <>
+    <div className="container">
       <h1>Danh sách bài viết</h1>
+      <hr />
       <div className="d-flex justify-content-end">
-        <Link href={"/admin/article/create"} className="btn btn-primary">Create</Link>
+        <Link href={"/admin/article/create"} className="btn btn-primary">Thêm bài viết</Link>
       </div>
       <ListArticle />
 
-    </>
+    </div>
   );
 };
 export default Article;
