201d28cfa0b34280634d4e7a24e538287d539f74
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-08-16 09:37:15 +0700

    kien

--- app/db.ts
+++ app/db.ts
@@ -4,12 +4,12 @@ let db: any;
 
 if (!db) {
   db = new Pool({
-    // user: process.env.PGSQL_USER,
-    // password: process.env.PGSQL_PASSWORD,
-    // host: process.env.PGSQL_HOST,
-    // port: 5432,
-    // database: process.env.PGSQL_DATABASE,
-    connectionString: 'postgres://postgres:123456@localhost:5432/ptbt',
+    user: process.env.PGSQL_USER,
+    password: process.env.PGSQL_PASSWORD,
+    host: process.env.PGSQL_HOST,
+    port: 5432,
+    database: process.env.PGSQL_DATABASE
+    // connectionString: 'postgres://postgres:123456@localhost:5432/ptbt',
   });
 }
 
